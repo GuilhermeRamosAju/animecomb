a7bd2df7731009a86902a2d3667e04d8528630bc
Guilherme Ramos <105815487+GuilhermeRamosAju@users.noreply.github.com>
2023-01-08 21:22:12 -0300

    refazendo o menu

--- script.js
+++
@@ -1,8 +0,0 @@
-const btnMenu = document.getElementById('menu-icon');
-
-function toggleMenu(){
-    const nav = document.getElementById('menu-opcoes');
-    nav.classList.toggle('active');
-}
-
-btnMenu.addEventListener('click', toggleMenu);
